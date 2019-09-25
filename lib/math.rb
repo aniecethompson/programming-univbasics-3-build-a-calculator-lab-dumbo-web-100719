582d659810cd4fa9ead12812df8e16d4cad01901
Aniece Thompson <aniecet@yahoo.com>
2019-09-25 16:02:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,20 +8,16 @@ end
 
 def division(num1, num2)
   num2 / num1
- 
 end
 
 def multiplication(num1, num2)
-  product= num1 * num2
-  puts product
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  modulo = num2 % num1
-  puts modulo
+  num2 % num1
 end
 
 def square_root(num)
-  root = Math.sqrt(num)
-  puts root
+  Math.sqrt(num)
 end
