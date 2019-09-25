6d051cdca3959df026591d7984c71c4cad83b92c
Aniece Thompson <aniecet@yahoo.com>
2019-09-25 15:54:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,16 @@
 def addition(num1, num2)
-sum = num1 + num2
-puts sum
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-difference = num2 - num1
-puts difference
+  difference = num2 - num1
+  puts difference
 end
 
 def division(num1, num2)
   quotient= num2 / num1
-puts quotient
+  puts quotient
 end
 
 def multiplication(num1, num2)
@@ -24,5 +24,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+  root = 
 end
